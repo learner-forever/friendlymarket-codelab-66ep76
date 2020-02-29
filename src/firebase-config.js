248bfeba21e3b1c72340de9c5a1a6c21d0c8dd83
learner-forever <neevahoza@gmail.com>
2020-02-29 13:51:41 +0530

    1 file modified

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,14 +2,16 @@
   Your Firebase config from the Firebase console 
   https://firebase.google.com/docs/web/setup#config-object
 */
-const firebaseConfig = {
-  apiKey: 'TODO',
-  authDomain: 'TODO',
-  databaseURL: 'TODO',
-  projectId: 'TODO',
-  storageBucket: 'TODO',
-  messagingSenderId: 'TODO',
-  appId: 'TODO'
-};
+  var firebaseConfig = {
+    apiKey: "AIzaSyCI1s6jLClGN3TqubiWrtDX9dZwWBNxkBY",
+    authDomain: "dsc3-389a2.firebaseapp.com",
+    databaseURL: "https://dsc3-389a2.firebaseio.com",
+    projectId: "dsc3-389a2",
+    storageBucket: "dsc3-389a2.appspot.com",
+    messagingSenderId: "191240384910",
+    appId: "1:191240384910:web:058867a958bc684101b4df"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 export default firebaseConfig;
